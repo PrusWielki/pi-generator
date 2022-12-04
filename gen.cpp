55a1a1399723f4d7ef4efb2886d6e2cad04d5d24
Patryk Prusak <pruswielki@gmail.com>
2022-12-04 15:21:33 +0100

    progress binary splitting

--- gen.cpp
+++ gen.cpp
@@ -8,6 +8,51 @@
 
 using namespace std;
 
+struct resultstruct {
+
+    mpf_class P, Q ,T;
+};
+
+
+
+resultstruct binary_splitting(int a, int b, mpf_class C324){
+
+     int m;
+     resultstruct result;
+
+     if(1==b-a){
+        if(0==a){
+        result.P=1;
+        result.Q=1;}
+    else{
+        result.P= (6*a-5)*(2*a-1)*(6*a-1);
+        result.Q=a*a*a*C324;
+    }
+    result.T=result.P*(13591409 + 545140134*a);
+    if(a&1){ // check if a is even
+        result.T=-1*result.T;
+    }
+
+     }
+     else{
+
+        m=(a+b)/2;
+        resultstruct result_a_m=binary_splitting(a,m,C324);
+        resultstruct result_m_b=binary_splitting(m,b,C324);
+
+        result.P=result_a_m.P+result_m_b.P;
+        result.Q=result_a_m.Q+result_m_b.Q;
+        result.T=result_a_m.T*result_m_b.Q+result_m_b.T*result_a_m.Q;;
+
+
+
+     }
+     return result;
+    
+    
+}
+
+
 int main(int argc, char** argv)
 {
     if(argc<=1) {
@@ -15,64 +60,34 @@ int main(int argc, char** argv)
         return 1;
      }
 
+    auto start = chrono::high_resolution_clock::now();
+
     int number_of_digits = atoi(argv[1]);
-    int precision =  log2(15)*(number_of_digits-2)+1;
-    int iterations = number_of_digits/14.18 +1;
+
+    double digits_per_iteration = log(53360^3) / log(10);
+
+    int precision =  log2(10) *number_of_digits; //log2(15)*(number_of_digits-2)+1;
+    int iterations = number_of_digits/digits_per_iteration +1;
 
     mpf_set_default_prec(precision);
 
-    mpf_class k, a_k, a_sum, b_sum, C, C3,C324, total, pi;
+    mpf_class C,C324, sqrtC;
+    mpf_class pi;
 
-   b_sum=0;
-   total=0;
-   pi=0;
-   k=1;
-   a_k=1;
-   a_sum=1;
-   C=640320;
-   C3=C*C*C;
-   C324=C3/24;
+    pi=0;
+    C=640320;
+    C324=C*C*C/24;
+    sqrtC=sqrt(C);
+
+
+
+     resultstruct result = binary_splitting(0,iterations,C324);
+
     
-
-    auto start = chrono::high_resolution_clock::now();
-
-     for(int i=0; i<iterations;i++)
-     // while(true)
-        {
-        // a_k *= -(6*k-5)*(2*k-1)*(6*k-1)
-        a_k *= -(6*k-5)*(2*k-1)*(6*k-1);
-        
-        // a_k /= k*k*k*C324
-        a_k = a_k/( k*k*k*C324);
-
-        // a_sum += a_k
-        a_sum += a_k;
-
-        // b_sum += k*a_k
-        b_sum += k*a_k;
-
-        // k+=1
-        k+=1;
+     pi = (result.Q*(mpf_class)426880*sqrtC)/result.T;
 
 
-        // check if we achieved the desired accuracy
-        //  mpf_mul_ui(eq1,a_k,number_of_digits*number_of_digits);
-        //  gmp_printf ("%.*Ff\n", number_of_digits, eq1);
-        //  mpf_floor(eq1,eq1);
-        //  gmp_printf ("%.*Ff\n", number_of_digits, eq1);
-        //  if(0 == mpf_cmp_ui(eq1,0))
-        //     break;
-
-    }
-    // total = 13591409*a_sum + 545140134*b_sum
-    total = 13591409*a_sum + 545140134*b_sum;
-
-    // pi = (426880*sqrt(10005*one, one)*one) / total
-    pi = (426880*sqrt((mpf_class)10005)) / total;
-
-    // gmp_printf ("%.*Ff", 10000, pi);
-
-
+     gmp_fprintf(stdout,"%d", result.T);
 
     FILE *f = fopen("file.txt", "w");
     if (f == NULL)
@@ -81,12 +96,14 @@ int main(int argc, char** argv)
         return 1;
     }
 
-     gmp_fprintf(f,"%.*Ff", number_of_digits, pi);
+      gmp_fprintf(f,"%.*Ff", number_of_digits, pi);
      // mpfr_out_str (f, 10, 0, pi, MPFR_RNDD);
+
+
 
     auto finish = chrono::high_resolution_clock::now();
     chrono::duration<double> elapsed = finish - start;
     printf("time: %f\n", elapsed.count());
-
+    
     return 0;
 }
