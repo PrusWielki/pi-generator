6dbb7a8401ba55d4f6a5bb07033cd96d63559931
Patryk Prusak <pruswielki@gmail.com>
2022-12-04 13:34:00 +0100

    #chudnovsky-gmpxx gmpxx

--- gen.cpp
+++ gen.cpp
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include <stdlib.h>
 #include <cmath>
-#include <gmp.h>
+#include <gmpxx.h>
 // #include <mpfr.h>
 // #include <mpf2mpfr.h>
 #include <chrono> 
@@ -15,44 +15,23 @@ int main(int argc, char** argv)
         return 1;
      }
 
-    // int one = 1;
-
-    // int precision = atoi(argv[1]);
     int number_of_digits = atoi(argv[1]);
-    // int amount_of_numbers= atoi(argv[3]);
-
     int precision =  log2(15)*(number_of_digits-2)+1;
-
     int iterations = number_of_digits/14.18 +1;
-
-    // printf("%d\n", precision);
-
 
     mpf_set_default_prec(precision);
 
-    mpf_t k, a_k, a_sum, b_sum, C, C3,C324, total, pi;
+    mpf_class k, a_k, a_sum, b_sum, C, C3,C324, total, pi;
 
-    mpf_inits(k, a_k, a_sum, b_sum,C, C3, C324, total, pi, NULL);
-
-    mpf_set_ui(b_sum,0);
-    mpf_set_ui(total,0);
-    mpf_set_ui(pi,0);
-
-    mpf_set_ui(k,1);
-    mpf_set_ui(a_k,1);
-    mpf_set_ui(a_sum,1);
-    mpf_set_ui(C,640320);
-
-    mpf_pow_ui(C3,C,3);
-    mpf_div_ui(C324,C3,24);
-    // mpf_floor(C324,C324);
-
-
-
-
-    mpf_t eq1, eq1_1, eq2, eq2_1, eq3, eq3_1;
-
-    mpf_inits(eq1, eq1_1, NULL);
+   b_sum=0;
+   total=0;
+   pi=0;
+   k=1;
+   a_k=1;
+   a_sum=1;
+   C=640320;
+   C3=C*C*C;
+   C324=C3/24;
     
 
     auto start = chrono::high_resolution_clock::now();
@@ -61,47 +40,19 @@ int main(int argc, char** argv)
      // while(true)
         {
         // a_k *= -(6*k-5)*(2*k-1)*(6*k-1)
-        // eq1 = -(6*k-5)
-        // eq1_1 = 6*k
-        // etc
-
+        a_k *= -(6*k-5)*(2*k-1)*(6*k-1);
         
-        mpf_mul_ui(eq1_1,k,6);
-        mpf_sub_ui(eq1,eq1_1,5);
-        mpf_neg(eq1,eq1);
-
-
-        mpf_mul(a_k,a_k,eq1);
-        
-        mpf_mul_ui(eq1_1,k,2);
-        mpf_sub_ui(eq1,eq1_1,1);
-
-        mpf_mul(a_k,a_k,eq1);
-        
-        mpf_mul_ui(eq1_1,k,6);
-        mpf_sub_ui(eq1,eq1_1,1);
-        
-        mpf_mul(a_k,a_k,eq1);
-
-
-
         // a_k /= k*k*k*C324
-        mpf_div(a_k,a_k,k);
-        mpf_div(a_k,a_k,k);
-        mpf_div(a_k,a_k,k);
-        mpf_div(a_k,a_k,C324);
-        // mpf_floor(a_k,a_k);
+        a_k = a_k/( k*k*k*C324);
 
         // a_sum += a_k
-        mpf_add(a_sum,a_sum,a_k);
+        a_sum += a_k;
 
         // b_sum += k*a_k
-        // eq1= k * a_k
-        mpf_mul(eq1, k, a_k);
-        mpf_add(b_sum,b_sum, eq1);
+        b_sum += k*a_k;
 
         // k+=1
-        mpf_add_ui(k,k,1);
+        k+=1;
 
 
         // check if we achieved the desired accuracy
@@ -114,20 +65,10 @@ int main(int argc, char** argv)
 
     }
     // total = 13591409*a_sum + 545140134*b_sum
-    // eq1 = 13591409*a_sum
-    // eq2 = 545140134*b_sum
-    mpf_mul_ui(eq1, a_sum, 13591409);
-    mpf_mul_ui(eq1_1, b_sum, 545140134);
-    mpf_add(total, eq1,eq1_1);
-
+    total = 13591409*a_sum + 545140134*b_sum;
 
     // pi = (426880*sqrt(10005*one, one)*one) / total
-    // eq1 = 426880*sqrt(10005)
-    // eq1_1 = sqrt(10005)
-    mpf_sqrt_ui(eq1_1,10005);
-    mpf_mul_ui(eq1,eq1_1, 426880);
-    mpf_div(pi,eq1,total);
-
+    pi = (426880*sqrt((mpf_class)10005)) / total;
 
     // gmp_printf ("%.*Ff", 10000, pi);
 
@@ -137,7 +78,6 @@ int main(int argc, char** argv)
     if (f == NULL)
     {
         printf("Error opening file!\n");
-        mpf_clears(k, a_k, a_sum, b_sum,C, C3, C324, total, pi, eq1, eq1_1, NULL);
         return 1;
     }
 
@@ -148,6 +88,5 @@ int main(int argc, char** argv)
     chrono::duration<double> elapsed = finish - start;
     printf("time: %f\n", elapsed.count());
 
-    mpf_clears(k, a_k, a_sum, b_sum,C, C3, C324, total, pi, eq1, eq1_1, NULL);
     return 0;
 }
